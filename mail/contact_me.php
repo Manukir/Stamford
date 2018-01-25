d57aec60e3a86dd49ac5a8a66e30e12615668c35
Manukir <manuelkir90@gmail.com>
2018-01-25 17:31:27 -0300

    arreglo form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,6 +4,7 @@ if(empty($_POST['name'])      ||
    empty($_POST['lastname'])  ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
+   empty($_POST['city'])      ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
